c4cb9ae979321e47a561de18882ebf40b868db46
Clincky <57742267+Clincky@users.noreply.github.com>
2022-09-05 00:11:27 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -35,7 +35,7 @@ export const config = {
       // 所在省份
       province: "江苏",
       // 所在城市
-      city: "句容",
+      city: "镇江",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: "https://pic1.zhimg.com/v2-0e5875681fabaee3e8fa7b2975d2f31b_r.jpg?source=1940ef5c"
     }, 
@@ -67,7 +67,7 @@ export const config = {
   // 默认所在省份, USERS 中没填的话, 会默认拿这里的省份
   PROVINCE: "江苏",
   // 默认所在城市, USERS 中没填的话, 会默认拿这里的城市
-  CITY: "句容",
+  CITY: "镇江",
 
   /** 重要节日相关 */
 
